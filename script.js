739a483ec22c5a34e6803ed700a020e0481aafc9
a h <aaron@4232media.com>
2019-05-31 13:58:59 -0400

    Updates

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 $( document ).ready(function() {
 	console.log( "ready!" );
 
-	$( ".awards-container .awards-item" ).hover(
-		function() {
-			console.log("over");
-			$( this ).find(".award-over").fadeOut();
-			$( this ).find(".award-active").fadeIn();
-			$( this ).find(".learn-more-button").fadeIn();
-		}, function() {
-			$( this ).find(".award-over").fadeIn();
-			$( this ).find(".award-active").fadeOut();
-			$( this ).find(".learn-more-button").fadeOut();
-
-		}
-	);
 
 
-	$(".country-wrapper").hover(
-		function() {
-			$(".sub-menu").css('display', 'flex');
-		}, function (){ 
-		//	$(".sub-menu").hide();
-		}
-	)
+	$(document).on('mouseenter', '.awards-container .awards-item', function () {
+		console.log("over");
+		$( this ).find(".award-over").fadeOut();
+		$( this ).find(".award-active").fadeIn();
+		$( this ).find(".learn-more-button").fadeIn();
+	  }).on('mouseleave', '.awards-container .awards-item', function () {
+		$( this ).find(".award-over").fadeIn();
+		$( this ).find(".award-active").fadeOut();
+		$( this ).find(".learn-more-button").fadeOut();	
+	});
 
-	$(".nav-wrapper").mouseleave(function(){
+	$(document).on('mouseenter', '.country-wrapper', function () {
+		$(".sub-menu").css('display', 'flex');
+	});
+
+	$(document).on('mouseleave', '.nav-wrapper', function () {
 		$(".sub-menu").hide();
 	});
+
+	// $( ".awards-container .awards-item" ).hover(
+	// 	function() {
+	// 		console.log("over");
+	// 		$( this ).find(".award-over").fadeOut();
+	// 		$( this ).find(".award-active").fadeIn();
+	// 		$( this ).find(".learn-more-button").fadeIn();
+	// 	}, function() {
+	// 		$( this ).find(".award-over").fadeIn();
+	// 		$( this ).find(".award-active").fadeOut();
+	// 		$( this ).find(".learn-more-button").fadeOut();
+
+	// 	}
+	// );
+
+
+	// $(".country-wrapper").hover(
+	// 	function() {
+	// 		$(".sub-menu").css('display', 'flex');
+	// 	}, function (){ 
+	// 	//	$(".sub-menu").hide();
+	// 	}
+	// )
+
+	// $(".nav-wrapper").mouseleave(function(){
+	// 	$(".sub-menu").hide();
+	// });
 
 	$(".learn-more-button").on("click", function() {
 		var dataTitle = $(this).data("award-title");
